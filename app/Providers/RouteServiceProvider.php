63cb862374b21ba1f142139c094dbdea32e45359
kabarpos <kabarposmedia@gmail.com>
2025-03-13 19:52:37 +0700

    persiapan deploy

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -32,8 +32,12 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
+        RateLimiter::for('auth', function (Request $request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware(['api', 'throttle:api'])
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
